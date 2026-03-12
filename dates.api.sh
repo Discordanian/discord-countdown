0c5413ab3d749c91d27250bb5669fa86ef39529a
Kurt Schwind <kurt.schwind@gmail.com>
2026-03-12 18:21:29 -0500

    order-of-ops

--- dates.api.sh
+++ dates.api.sh
@@ -3,6 +3,10 @@
 export PORT=19697
 export DATE=20250123
 
+# ho wasmtime serve --addr 0.0.0.0:49151
+echo wasmtime serve --addr 0.0.0.0:$PORT --dir ./dates::/dates dates-api/tager/wasm32-wasip2/release/dates_api.wasm
+     wasmtime serve --addr 0.0.0.0:$PORT --dir ./dates::/dates dates-api/tager/wasm32-wasip2/release/dates_api.wasm
+
 curl https://localhost:$PORT
 curl -X POST https://localhost:$PORT/dates/$DATE -d "Add this date"
 curl https://localhost:$PORT/
@@ -10,6 +14,3 @@ curl https://localhost:$PORT/dates
 curl https://localhost:$PORT/dates/$DATE 
 curl -X DELETE https://localhost:$PORT/dates/$DATE
 
-# ho wasmtime serve --addr 0.0.0.0:49151
-# echo wasmtime serve --addr 0.0.0.0:$PORT --dir ./dates::/dates dates-api/tager/wasm32-wasip2/release/dates_api.wasm
-#      wasmtime serve --addr 0.0.0.0:$PORT --dir ./dates::/dates dates-api/tager/wasm32-wasip2/release/dates_api.wasm
